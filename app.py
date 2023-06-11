0c9d7dd5d4060bf9392ad0257e52bedf74824734
talaa <tamer.alaaeldine@gmail.com>
2023-06-12 02:36:25 +0300

    Adding the requirements +utilities

--- app.py
+++ app.py
@@ -1,7 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return render_template("index.html", title="Hello")
+
+
+@app.route('/ai-financialnews-sentiment-analysis', methods=['POST'])
+def process():
+    text_input = request.form.get('stock_name')
+    slider_input = request.form.get('days_input')
+
+    # Use the values as needed
+    print(f'Text Input: {text_input}')
+    print(f'Slider Input: {slider_input}')
+
+    # Perform other operations or return a response if needed
+
+    return 'Form submitted!'
+
+    
